0f01b4db15bcd26ca99ed39b2e6709c1230d206b
Sally Lee-Duffy <sally@thesnowinstitute.com>
2024-04-20 11:25:45 +0200

    Added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiURL);
+    axios.get(apiURL).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
