96a2ce32c9afdb68a43f96c489331a629e96395e
Sally Lee-Duffy <sally@thesnowinstitute.com>
2024-05-06 16:15:13 +0200

    Added Photos JS and CSS to our App

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiURL).then(handleResponse);
+    axios.get(apiURL).then(handleDictionaryResponse);
+
+    let pexelsApiKey = "f1ef93f40de3d662400b2dt0aa8a2o91";
+    let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}`;
+    axios.get(pexelsApiUrl).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -46,6 +56,7 @@ export default function Dictionary(props) {
             suggested words: sunset, phone, rain.......
           </div>
           <Results results={results} />
+          <Photos photos={photos} />
         </section>
       </div>
     );
